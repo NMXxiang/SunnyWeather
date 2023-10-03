260624a1d5c0deaabf536fce5e941518daad3362
fox <a88667733zx@163.com>
2023-10-03 22:09:20 +0800

    实现搜索全球城市数据功能

---
+++ app/src/main/java/com/example/sunnyweather/logic/Repository.kt
@@ -0,0 +1,27 @@
+package com.example.sunnyweather.logic
+
+import android.util.Log
+import androidx.lifecycle.liveData
+import com.example.sunnyweather.logic.model.Place
+import com.example.sunnyweather.logic.network.SunnyWeatherNetwork
+import kotlinx.coroutines.Dispatchers
+
+object Repository {
+    fun searchPlaces(query: String) = liveData(Dispatchers.IO) {
+        val result = try {
+            val placeResponse = SunnyWeatherNetwork.searchPlaces(query)
+            Log.d("placeResponse",placeResponse.toString())
+            if (placeResponse.status == "ok") {
+                Log.d("placeResponse.status",placeResponse.status)
+                val places = placeResponse.places
+                Result.success(places)
+            } else {
+                Result.failure(RuntimeException("response status is${placeResponse.status}"))
+            }
+        } catch (e: Exception) {
+            Result.failure<List<Place>>(e)
+        }
+        emit(result)
+    }
+
+}
